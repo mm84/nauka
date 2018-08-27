23d1b47d127046c1d60edbedc24468dc821efe69
mlyna <mlyna@MARCIN-ZEN>
2018-08-27 15:32:11 +0200

    dss

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp2
 {
@@ -10,6 +6,74 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            //NewMethod();
+
+            for (; ; )
+            {
+                Przywitanie();
+                Pytanieowiek();
+                Ustawienia();
+            }
+        }
+
+        //private static void NewMethod()
+        //{
+        //    int maxInt = int.MaxValue;
+        //    int minInt = int.MinValue;
+        //    long maxLong = long.MaxValue;
+        //    long minLong = long.MinValue;
+
+        //    Console.WriteLine("maxInt =" + maxInt);
+        //    Console.WriteLine("minInt =" + minInt);
+        //    Console.WriteLine("maxLong =" + maxLong);
+        //    Console.WriteLine("minLong =" + minLong);
+        //}
+
+        /// <summary>
+        /// Czyszczenie ustawień
+        /// </summary>
+        private static void Ustawienia()
+        {
+            Console.ReadKey();
+            Console.Clear();
+            Console.ResetColor();
+        }
+        /// <summary>
+        /// Pytanie o wiek
+        /// </summary>
+        private static void Pytanieowiek()
+        {
+            Console.WriteLine("Ile masz lat?");
+            int age;
+            bool result = int.TryParse(Console.ReadLine(), out age);
+
+
+            if (age > 18)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("Brawo, możesz wypić piwo");
+
+            }
+            else if (result == false)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Wprowadz poprawny wiek");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("możesz wypić mleko");
+
+            }
+        }
+        /// <summary>
+        /// Powitanie
+        /// </summary>
+        private static void Przywitanie()
+        {
+            Console.WriteLine("Wprowadź swoje imie");
+            string name = Console.ReadLine();
+            Console.WriteLine("Witaj, " + name);
         }
     }
 }
---
+++ Student/dziennik.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Student
+{
+    class dziennik
+    {
+        //stan
+        List<float> oceny;
+
+        //Zachownia
+        public void dodawanieOcen(float ocena)
+        {
+            oceny.Add(ocena);
+        }
+         
+        public float Obliczanieslredniej()
+        {
+            float sum = 0, avg = 0;
+
+            foreach (var ocena in oceny)
+            {
+                sum = sum + ocena;
+            }
+
+            avg = sum / oceny.Count();
+
+            return avg;
+
+        }
+
+        public float maxOcena()
+        {
+            return oceny.Max();
+        }
+
+        public float minOcena()
+        {
+            return oceny.Min();
+
+        }
+    }
+}
